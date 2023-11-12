0f23601b744e4a9d345ba64fa6b1c29d8530cd27
Merdan <babayevmerdan20@gmail.com>
2023-11-12 19:16:08 +0300

    Evrythings update

--- hizmetler/views.py
+++ hizmetler/views.py
@@ -94,31 +94,6 @@ def yorum(request, id):
             return redirect('giris')
     return redirect(reverse("hizmet:detay",kwargs={"id":id}))
 
-# def artikle_likes(request, id):
-#     post = get_object_or_404(Article, id=request.POST.get('blogpost_id'))
-#     if post.likes.filter(id=request.user.id).exists():
-#         post.likes.remove(request.user)
-#     else:
-#         post.likes.add(request.user)
-
-#     return HttpResponseRedirect(reverse('hizmet:detay', args=[id]))
-
-# class BlogPostDetailView(DetailView):
-#     model = Article
-#     # template_name = MainApp/BlogPost_detail.html
-#     # context_object_name = 'object'
-
-#     def get_context_data(self, **kwargs):
-#         data = super().get_context_data(**kwargs)
-
-#         likes_connected = get_object_or_404(Article, id=self.kwargs['pk'])
-#         liked = False
-#         if likes_connected.likes.filter(id=self.request.user.id).exists():
-#             liked = True
-#         data['number_of_likes'] = likes_connected.number_of_likes()
-#         data['post_is_liked'] = liked
-#         return data
-        
 
 def artikle_likes(request, id):
      if request.user.is_authenticated:
